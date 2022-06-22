054d70d34e9c4030dd6011c0a053248bb8f3a48c
CDW1234235562 <18868716309@163.com>
2022-06-22 20:56:05 +0800

    i'm a idiot

--- flask_app.py
+++ flask_app.py
@@ -6,7 +6,7 @@ filepath = '/home/cdw1234235562/Algor/2016Census_P01_NSW_SA2.csv'
 sa2 = pd.read_csv(filepath)
 
 Tot_P_P = sa2['Tot_P_P']
-SA2_MAINCODE = sa2['SA2_MAINCODE_2016']
+SA2_MAINCODE = sa2['SA2_MAINCODE_2016'].tolist()
 
 app = Flask(__name__)
 
@@ -19,8 +19,8 @@ def hello_world():
 @app.route('/Tot_P_P/<int:sa2_code>')
 def population(sa2_code):
     if sa2_code in SA2_MAINCODE:
-        index = sa2[sa2["SA2_MAINCODE_2016"] == 101021007].index.tolist()[0]
-        total_population = sa2.iloc[index, [3]]
-        return('that is a NSW SA2 code!, population is:', total_population)
+        index = sa2[sa2["SA2_MAINCODE_2016"] == sa2_code].index.tolist()[0]
+        total_population = int(sa2.iloc[index, [3]])
+        return 'that is a NSW SA2 code!, population is: %s' % total_population
     else:
-        return('you type the wrong code!')
+        return 'you type the wrong code!'

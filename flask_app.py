5a0cb5e890f044ccd89916688a36cda47547426b
CDW1234235562 <18868716309@163.com>
2022-06-22 18:36:34 +0800

    add Tot_P_P search fuction

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,12 @@
 from flask import Flask
 import rhino3dm as rhino
+import pandas as pd
+
+
+sa2 = pd.read_csv('2016Census_P01_NSW_SA2.csv')
+
+Tot_P_P = sa2['Tot_P_P']
+SA2_MAINCODE = sa2['SA2_MAINCODE_2016']
 
 app = Flask(__name__)
 
@@ -9,6 +16,12 @@ def hello_world():
     return 'Link start!'
 
 
-@app.route('/urlend')
-def username():
-    return {"username": 'Kirito', "password": '20170526'}
+@app.route('/Tot_P_P/<sa2_code>')
+def population(sa2_code):
+    for sa2_code in SA2_MAINCODE:
+        if (SA2_MAINCODE == sa2_code):
+            total_population = sa2.iloc[sa2_code, [3]]
+            return ('that is a NSW SA2 code!'+'population is' + total_population)
+
+        else:
+            return 'you type the wrong code!'

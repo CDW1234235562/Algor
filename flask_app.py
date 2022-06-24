18821aa5a6adf42a1210491708bce26fb08406b8
CDW1234235562 <18868716309@163.com>
2022-06-24 13:51:00 +0800

    fixed!

--- flask_app.py
+++ flask_app.py
@@ -18,22 +18,12 @@ def hello_world():
     return 'Link start!'
 
 
-@app.route('/Tot_P_P/<int:sa2_code>')
-def population(sa2_code):
-    if sa2_code in SA2_MAINCODE:
-        index = sa2[sa2["SA2_MAINCODE_2016"] == sa2_code].index.tolist()[0]
-        total_population = int(sa2.iloc[index, [3]])
-        return 'that is a NSW SA2 code!, population is: %s' % total_population
-    else:
-        return 'you type the wrong code!'
-
-
 @hops.component(
     "/hops_sa2_population",
     name="sa2_population",
     description="Get sa2 block population",
     inputs=[
-        hs.HopsNumber("Sa2_code", "Sa2", "SA2 block MAINCODE 2016ver."),
+        hs.HopsNumber("Sa2_code", "Sa2_code", "Parameter on Curve to evaluate"),
     ],
     outputs=[
         hs.HopsNumber("Total_population", "Tot_P", "Total population on this SA2 block")
@@ -43,7 +33,7 @@ def pointat(Sa2_code):
     if Sa2_code in SA2_MAINCODE:
         index = sa2[sa2["SA2_MAINCODE_2016"] == Sa2_code].index.tolist()[0]
         population = int(sa2.iloc[index, [3]])
-    return hs.HopsNumber(population)
+    return population
 
 
 if __name__ == "__main__":

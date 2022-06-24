07a203f7c5a00bb7d92bb45f93b2753c45d18994
CDW1234235562 <18868716309@163.com>
2022-06-24 12:56:45 +0800

    hops adp

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import rhino3dm as rhino
 import pandas as pd
+import ghhops_server as hs
 
 filepath = '/home/cdw1234235562/Algor/2016Census_P01_NSW_SA2.csv'
 sa2 = pd.read_csv(filepath)
@@ -9,6 +10,7 @@ Tot_P_P = sa2['Tot_P_P']
 SA2_MAINCODE = sa2['SA2_MAINCODE_2016'].tolist()
 
 app = Flask(__name__)
+hops = hs.Hops(app)
 
 
 @app.route('/')
@@ -24,3 +26,25 @@ def population(sa2_code):
         return 'that is a NSW SA2 code!, population is: %s' % total_population
     else:
         return 'you type the wrong code!'
+
+
+@hops.component(
+    "/hops_sa2_population",
+    name="sa2_population",
+    description="Get sa2 block population",
+    inputs=[
+        hs.HopsNumber("Sa2_code", "Sa2", "SA2 block MAINCODE 2016ver."),
+    ],
+    outputs=[
+        hs.HopsNumber("Total_population", "Tot_P", "Total population on this SA2 block")
+    ]
+)
+def pointat(Sa2_code):
+    if Sa2_code in SA2_MAINCODE:
+        index = sa2[sa2["SA2_MAINCODE_2016"] == Sa2_code].index.tolist()[0]
+        population = int(sa2.iloc[index, [3]])
+    return hs.HopsNumber(population)
+
+
+if __name__ == "__main__":
+    app.run()
